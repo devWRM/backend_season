0c5bb8f12fe043a715d60e6c9cde6d73b33cd43a
devWRM <mcwen@comcast.net>
2021-09-17 17:39:14 -0400

    added dependent destroy & validations

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,4 +1,7 @@
 class Term < ApplicationRecord
-    has_many :months
+    has_many :months, :dependent => :destroy
     has_many :traits, through: :months
+
+    validates :isActive, :name, presence: true
+
 end
