a7e124f08186e17f93247d63259bcf02351c790f
devWRM <mcwen@comcast.net>
2021-09-17 17:01:06 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  
+  resources :terms do
+    resources :traits
+  end
+
+  resources :months do
+    resources :traits
+  end
+
   resources :traits
-  resources :months
-  resources :terms
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
